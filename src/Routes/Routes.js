217469d02cd317ab349ead47cff899318e3c363f
eti kona <etikonapal@gmail.com>
2022-10-26 01:55:02 +0600

    tailwind and daisy ui added

--- src/Routes/Routes.js
+++ src/Routes/Routes.js
@@ -1,13 +1,14 @@
 import { Children } from "react";
 import { createBrowserRouter } from "react-router-dom";
 import Main from "../layouts/Main";
+import Header from "../Shared/Header";
 
 export const routes = createBrowserRouter([
     {
     path: '/',
     element: <Main/>,
     children: [{
-        
+       
     }]
 }
 ])
--- src/layouts/Main.js
+++ src/layouts/Main.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
+import Footer from '../Shared/Footer';
+import Header from '../Shared/Header';
 
 const Main = () => {
     return (
         <div>
+            <Header/>
             <Outlet/>
+            <Footer/>
         </div>
     );
 };
