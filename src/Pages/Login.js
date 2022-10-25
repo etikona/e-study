f20347a2ab350b1937025da0bf388ee422ec73a3
eti kona <etikonapal@gmail.com>
2022-10-26 02:51:21 +0600

    sign in and login page ready

--- src/Pages/Login.js
+++ src/Pages/Login.js
@@ -1,9 +1,54 @@
-import React from 'react';
-import './Login.css';
+import React, { useContext, useState } from 'react';
+import { Link, Navigate, useLocation, useNavigate } from 'react-router-dom';
+import { AuthContext } from '../contexts/UserContext';
+
+import './Login.css'
 const Login = () => {
+    const [error, setError] = useState(null);
+    const{userLogIn} = useContext(AuthContext);
+    const navigate = useNavigate();
+    const location = useLocation();
+    const from = location.state?.from?.pathname || '/';
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        const form = event.target;
+        const email = form.email.value;
+        const password = form.password.value;
+        // Error displaying
+        if(password.length < 6){
+            setError("You are given wrong password!")
+            return;
+        }
+      
+        // Creating user
+        userLogIn(email, password)
+        .then(res => {
+            const user = res.user;
+            form.reset()
+            navigate(from, {replace: true});
+        })
+        .catch(err =>{
+            const msg = err.message;
+        } )
+    }
+
     return (
-        <div>
-            <h2>This is login page</h2>
+        <div className='form-container'>
+            <h3 className='form-title'>Login</h3>
+            <form onSubmit={handleSubmit}>
+                <div className="form-control">
+                    <label htmlFor="email">Email</label>
+                    <input type="email" name="email" required id="" />
+                </div>
+                <div className="form-control">
+                    <label htmlFor="password">Password</label>
+                    <input type="password" name="password" required id="" />
+                </div>
+                <input className='btn-submit' type="submit" value="Login" />
+            </form>
+            <p className='err-text'>{error}</p>
+            <p><Link to='/register'>create a new account</Link></p>
         </div>
     );
 };
--- src/Pages/Signin.js
+++ src/Pages/Signin.js
@@ -1,10 +1,60 @@
-import React from 'react';
-import './Signin.css';
+import React, { useContext, useState } from 'react';
+import { Link } from 'react-router-dom';
+import { AuthContext } from '../contexts/UserContext';
+
+import './Signin.css'
+
 const Signin = () => {
+    const[error, setError] = useState(null);
+    const {createUser} = useContext(AuthContext)
+
+    const handleSubmit = (event)=>{
+        event.preventDefault();
+        const form = event.target;
+        const email = form.email.value;
+        const password = form.password.value;
+        const confirm = form.confirm.value;
+        // Error displaying
+        if(password.length < 6){
+            setError("please give at least 6 characters")
+            return;
+        }
+        if(password !== confirm){
+            setError("You have entered wrong password")
+            return;
+        }
+        // Creating user
+        createUser(email, password)
+        .then(res => {
+            const user = res.user;
+            form.reset()
+            console.log(user);
+        })
+        .catch(err =>{
+            const msg = err.message;
+        } )
+    }
     return (
-        <div>
-            <h2>This is signin page</h2>
-        </div>
+        <div className='form-container'>
+        <h3 className='form-title'>Sign In</h3>
+        <form onSubmit={handleSubmit}>
+        <div className="form-control">
+                <label htmlFor="email">Email</label>
+                <input type="email" name="email" required id="" />
+            </div>
+            <div className="form-control">
+                <label htmlFor="password">Password</label>
+                <input type="password" name="password" required id="" />
+            </div>
+            <div className="form-control">
+                <label htmlFor="password">Confirm password</label>
+                <input type="password" name="confirm" required id="" />
+            </div>
+            <input className='btn-submit' type="submit" value="Sign in" />
+        </form>
+        <p className='err-text'>{error}</p>
+        <p>All ready have an account?<Link to='/login'>Login</Link></p>
+    </div>
     );
 };
 
