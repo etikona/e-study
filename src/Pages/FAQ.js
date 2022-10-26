0e113641dce6cef4baf826a4fa74f43caef87fcf
eti kona <etikonapal@gmail.com>
2022-10-27 04:17:49 +0600

    FAQ added

--- src/Pages/FAQ.js
+++ src/Pages/FAQ.js
@@ -3,7 +3,31 @@ import React from 'react';
 const FAQ = () => {
     return (
         <div>
-            <h3>Your questions are in FAQ section</h3>
+          
+          <div className="dropdown dropdown-end grid"><font></font>
+  <label tabIndex={0} className="btn m-1">Are E-study courses Online or Offline?</label><font></font>
+  <ul tabIndex={0} className="dropdown-content menu p-2 shadow bg-base-100 rounded-box w-52"><font></font>
+    <li><a>E-study is a 100% e-Learning platform and we only provide online courses.</a></li><font></font>
+   
+  </ul><font></font>
+  <label tabIndex={0} className="btn m-1">Do you provide video recording of live classes?</label><font></font>
+  <ul tabIndex={0} className="dropdown-content menu p-2 shadow bg-base-100 rounded-box w-52"><font></font>
+    <li><a>Yes, we provide recording with some conditions</a></li><font></font>
+   
+  </ul><font></font>
+  <label tabIndex={0} className="btn m-1">Do you provide certificates after completing a course?</label><font></font>
+  <ul tabIndex={0} className="dropdown-content menu p-2 shadow bg-base-100 rounded-box w-52"><font></font>
+    <li><a>we will provide digital certificate to passing students.</a></li><font></font>
+   
+  </ul><font></font>
+  <label tabIndex={0} className="btn m-1">Do you provide job placement upon passing course?</label><font></font>
+  <ul tabIndex={0} className="dropdown-content menu p-2 shadow bg-base-100 rounded-box w-52"><font></font>
+    <li><a>Yes, we provide help in job placement in selected courses.</a></li><font></font>
+   
+  </ul><font></font>
+</div>
+  
+
         </div>
     );
 };
